0c2837f614428cfe73ed4e110019115260ec4cec
AlundraOz <ailuozols@gmail.com>
2020-05-11 00:18:54 +0200

    app working ,missing end and stringify

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ class App extends React.Component {
     correctAnswer: [],
     endGame: false,
     current_Index: 0,
-    answers: [],
-    userAnswer: [],
     score: 0,
   };
 
@@ -21,62 +19,63 @@ class App extends React.Component {
     const response = await fetch(url);
     const data = await response.json();
     let current_Index = this.state.current_Index;
-    let {currentIndex} = this.state
+    //const myArrStr = JSON.stringify(myArr);
       this.setState({ 
-        data: data.results[current_Index] ,
+        data: pregunta ,
         loading: false,
         questions: data.results,
         rightAnswer: data.results[current_Index].correct_answer,
+        current_Index: this.state.current_Index + 1
         })
-        console.log(data)
-        console.log(this.state.questions);
+ //      console.log(data)
+ //       console.log(this.state.questions);
   }
 
  selectOption  = (event) => {
   const {rightAnswer} = this.state
-  console.log(rightAnswer)
-  console.log(event.target.id)
+ // console.log(rightAnswer)
+ // console.log(event.target.id)
   /* Guardamos respuesta del usuario */
   var response = (event.target.id === "btn-true") ? "True" : "False";
   
-  console.log(response)
+  //console.log(response)
   /* Revisamos si la respuesta es correcta y sumamos un puntito */
-  if(rightAnswer == response){
+  if(rightAnswer === response){
     console.log('iesaaa ganaste');
     this.setState ({score: this.state.score + 1})
+    //añadir clase success al boton
+  }else{
+    //añadir clase error al boton
   }
-  /*mientras que el indice sea menor igual a 10 index+1*/
-  console.log(this.state.current_Index)
+
+  //set time out -> para ejecutar la siguiente pregunta si corresponde 
+  setTimeout( 
+    function() {
+      this.nextQuestion();
+    }.bind(this), 2000);
+  
+ }
+
+ nextQuestion = () => {
+    /*mientras que el indice sea menor igual a 10 index+1*/
+  // console.log(this.state.current_Index)
   if (this.state.current_Index < 9) {    
     /* questions /* las preguntas 10 */
     /* data /*es la pregunta actual*/
     /* rightAnswer: data.results[current_Index].correct_answer, */
+    let index = this.state.current_Index + 1;
     this.setState({
-      current_Index: this.state.current_Index +1,
+      current_Index: index,
+      //data pasa a ser la siguiente pregunta (siguiente posicion de indice)
       data: this.state.questions[this.state.current_Index],
       rightAnswer: this.state.questions[this.state.current_Index].correct_answer
-      
     })
-    /*
-    let myIndex = this.state.currentIndex++; 
-    this.setState({currentIndex: myIndex})  
-    */
   }else{
     //mostrar puntuacion! end game --- todos felices
     console.log(this.state.score)
-
   }
-  console.log(this.state.current_Index)
  }
 
- nextQuestion = () => {
-  const {rightAnswer} = this.state
-  (rightAnswer) ? 
-    this.setState({ score: this.state.score + 1, 
-      current_Index: this.state.currentIndex + 1 })
-               : this.setState({current_Index: this.state.current_Index + 1})
- 
-    }
  render() {
    return(
      <div>
